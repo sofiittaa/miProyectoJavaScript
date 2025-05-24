422c2bd4b0a82dc4ee2f79a9d5ffe4490a70afd8
Sofia Furtado <sakuramin783@gmail.com>
2025-05-24 01:11:15 -0300

    si

---
+++ BajaVibe/JavaScript.js
@@ -0,0 +1,22 @@
+const carrito = [];
+
+const producto1 = {
+    nombre: "Flared Jean Tiro Bajo",
+    precio: 999
+}
+
+const producto2 = {
+    nombre: "Jean recto Tiro bajo",
+    precio: 1499
+}
+
+function comprarPantalon1(producto1) {
+    const confirmar = confirm(`¿Queres comprar ${producto1} por $${precio}?`);
+
+    if (confirm) {
+        let cantidad = prompt("¿Cuantas unidades desea comprar?")
+        cantidad = parseInt(cantidad);
+
+    }
+}
+
