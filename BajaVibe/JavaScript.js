d595d9e1120322f063aa5a5f41b6a1eefa983ddb
Sofia Furtado <sakuramin783@gmail.com>
2025-05-24 11:19:22 -0300

    Pre entrega 1

--- BajaVibe/JavaScript.js
+++ BajaVibe/JavaScript.js
@@ -1,22 +1,23 @@
-const carrito = [];
+const productos = [
+    { nombre: "Flared Jean Tiro Bajo", precio: 999 },
+    { nombre: "Jean recto Tiro bajo", precio: 1499 }
+];
 
-const producto1 = {
-    nombre: "Flared Jean Tiro Bajo",
-    precio: 999
+const botones = document.getElementsByClassName('btn-comprar');
+
+for (let i = 0; i < botones.length; i++) {
+    botones[i].addEventListener("click", function () {
+        let confirmar = prompt(`¿Quieres comprar este pantalón ${productos[i].nombre}? (si/no)`);
+
+        if (confirmar === "si" || confirmar === "Si") {
+            console.log(`${productos[i].nombre} agregado al carrito.`);
+            alert(`${productos[i].nombre} agregado al carrito.`);
+        } else if (confirmar === "no" || confirmar === "No") {
+            console.log(`Cancelaste la compra de ${productos[i].nombre}`);
+            alert(`Cancelaste la compra de ${productos[i].nombre}`);
+        } else {
+            console.log("Respuesta no válida.");
+            alert("Por favor responde con 'si' o 'no'.");
+        }
+    });
 }
-
-const producto2 = {
-    nombre: "Jean recto Tiro bajo",
-    precio: 1499
-}
-
-function comprarPantalon1(producto1) {
-    const confirmar = confirm(`¿Queres comprar ${producto1} por $${precio}?`);
-
-    if (confirm) {
-        let cantidad = prompt("¿Cuantas unidades desea comprar?")
-        cantidad = parseInt(cantidad);
-
-    }
-}
-
